5f505e148cea613a4ca81f756f5fd2d7d714d99b
Ocelot <100012348+billycwong19@users.noreply.github.com>
2022-04-07 00:18:55 -0500

    update of js and index to clean things up

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,10 @@ var timeBlockList = document.querySelector(".timeBlockList")
 var currentTime = document.querySelector("#currentDay")
 var dateAndTime = moment().format("MMMM Do YYYY");
 currentTime.textContent = dateAndTime;
+var currentHour = moment().hour();
 
-var hoursOfTheDay = ["9am", "10am", "11am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm","7pm", "8pm", "9pm"]
+
+var hoursOfTheDay = ["9", "10", "11", "12", "13", "14", "15", "16", "17", "18","19", "20", "21"]
 var saveBtn;
 
 function generator() {
@@ -12,28 +14,60 @@ function generator() {
         var timeBlock = $('<div>');
             timeBlock.addClass('container-fluid timeBlock vw-100 row')
             timeBlock.insertAfter(timeBlockList);
-        var hour = $("<h3>")
+        var hour = $("<h3 class= " + hoursOfTheDay[i] + ">")
             hour.addClass('hour col-3 col-md-2 col-lg-1')
-            hour.text(hoursOfTheDay[i]);
+            if (hoursOfTheDay[i] > 12){
+                hour.text((hoursOfTheDay[i] - 12) + " PM");
+            } else {
+                hour.text(hoursOfTheDay[i] + " AM");
+            }
             timeBlock.append(hour);
         var activity = $("<input type=text>")
-            activity.addClass('activity col-6 col-md-8 col-lg-10 bg-info')
+            activity.addClass('activity col-6 col-md-8 col-lg-10')
             activity.insertAfter(hour)
         var saveBtn = $("<button>")
-            saveBtn.addClass('saveBtn col-3 col-md-2 col-lg-1 bg-dark')
+            saveBtn.addClass('saveBtn col-3 col-md-2 col-lg-1')
             saveBtn.insertAfter(activity)
         var icon = $("<i>")
             icon.addClass("fas fa-save")
             saveBtn.append(icon)
-      }
-      saveBtn.on("click", save)
+        }
+        compare();
     }
-function save(){
-    var input = $("activity").value
-    console.log(input)
+
+function compare(){
+    var activity = $('.activity')
+for (let i = 0; i < hoursOfTheDay.length; i++){
+    if (currentHour === hoursOfTheDay[i]){
+        activity.addClass('present')
+      
+    } else if (currentHour > hoursOfTheDay[i]){
+        activity.addClass('past')
+     
+    } else {
+        activity.addClass('future')
+        }
+    }
 }
+
+function save(){
+    var activity = $('.activity')
+    var input = activity.text();
+    input = localStorage.setItem(JSON.stringify(right))
+}
+
+function logActivity(event) {
+    var activity = $('.activity')
+    var input = activity.text();
+    var target = $(event.target);
+    
+  }
+timeBlockList.click(logActivity);
+  
 function init(){
     generator();
 }
+
 init();
 
+$(".saveBtn").on("click", save)
